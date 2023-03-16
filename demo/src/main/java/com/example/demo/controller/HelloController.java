15ee6268fbd45d3aef07c4952dc02c8327c947ea
hanjunhee <hjh0307123@naver.com>
2023-03-16 11:08:53 +0900

    로또 번호 생성

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,24 +3,30 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/test")
 public class HelloController {
 
-	@GetMapping("hello")
+	@GetMapping("/hello")
 	public String hello(Model model) {
 
 		model.addAttribute("data", "hello!!");
-		
-		model.addAttribute("aaa", "병신아");
+
+		model.addAttribute("aaa", "ㅋㄷㅋㄷㅋㄷㄷ");
 
 		return "hello";
 	}
 
-	@GetMapping("hello-mvc")
-	public String helloMvc(@RequestParam("name") String name, Model model) {
+
+	@GetMapping("/hello-mvc")
+	public String helloMvc( Model model) {
+
+		String name = "한준희";
 
 		System.out.println("name        ////    " + name);
 
@@ -29,11 +35,12 @@ public class HelloController {
 		return "hello-template";
 	}
 
+
 	@GetMapping("hello-string")
 	@ResponseBody
 	public String helloString(@RequestParam("name") String name) {
-		
-		
+
+
 		return "hello " + name;
 	}
 
